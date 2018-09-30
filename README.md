3d4ab3253a3e6601696050d071085cc62bdfeded
Chris Hafey <chafey@gmail.com>
2018-09-30 07:10:11 -0500

    added one way rsync

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # vagrant-gatsby
-Vagrantfile for a gatsby VM with vmware fusion
+Vagrantfile for a [gatsby](https://www.gatsbyjs.org/)  with support for vmware vagrant plugin
+
+NOTE: you need to keep vagrant rsync-auto running to make sure source changes on host are copied to the guest:
+
+Terminal One:
+> vagrant up
+> vagrant rsync-auto
+
+Terminal Two:
+> vagrant ssh
+> gatsby new testApp
+> cd testApp
+> gatsby develop -H 0.0.0.0
+
+Open the gatsby app in your web browser running on the host:
+> open http://localhost:8000/
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,24 @@
 #! /bin/bash
+
+# turn off apt errors "dpkg-preconfigure: unable to re-open stdin: No such file or directory"
+export DEBIAN_FRONTEND=noninteractive
+
+#update apt
 apt-get update
+
+#make sure we have up to date versions of git and curl 
 apt-get install git
 apt-get install curl
+
+#instlal nodejs 10
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 apt-get install -y nodejs
+
+#get latest npm
 npm install npm --global
+
+#install gatsby
 npm install -g gatsby-cli
+
+#install rsync
+apt-get install -y rsync
